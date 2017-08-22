c69e150cd91d997b23e9070b65c4430a5ea9f347
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:01:07 -0700

    msm8998-common: Move Lineage specific overlays to overlay-lineage
    Change-Id: Ic38bb1acb4d37f90b62679e4420815f49b30fc7f

--- msm8998.mk
+++ msm8998.mk
@@ -15,7 +15,9 @@
 #
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
