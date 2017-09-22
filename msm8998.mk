0f4bd1a554f64ae2a9395166f4f396f5e653c3f9
Yifan Hong <elsk@google.com>
2017-09-22 15:16:18 -0700

    msm8998-common: Uprev health to 2.0
    Test: adb logcat -e health
    Test: lshal --debug
    
    Change-Id: I3b14f087d95578d034898600592cb6fb357a9958

--- msm8998.mk
+++ msm8998.mk
@@ -197,8 +197,8 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-service \
+    chargeonlymode
 
 # HIDL
 PRODUCT_PACKAGES += \
