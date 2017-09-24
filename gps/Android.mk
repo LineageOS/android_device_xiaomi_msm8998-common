224c4a4905a1bd59e9dc273ffcc51c3477b42306
Michael Bestas <mkbestas@lineageos.org>
2017-09-24 20:38:07 +0300

    msm8998-common: gps: Cleanup makefiles and remove unused loc_api
    Change-Id: Iea3d329dd5f04598d1994eec1f626d9b95b08502

--- gps/Android.mk
+++ gps/Android.mk
@@ -16,4 +16,4 @@
 
 LOCAL_PATH := $(call my-dir)
 
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+include $(call all-subdir-makefiles)
--- gps/utils/platform_lib_abstractions/loc_pla/src/Android.mk
+++ gps/utils/platform_lib_abstractions/loc_pla/src/Android.mk
@@ -1,6 +1,3 @@
-ifneq ($(BUILD_TINY_ANDROID),true)
-#Compile this library only for builds with the latest modem image
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -25,7 +22,6 @@ LOCAL_CFLAGS += \
      -D_ANDROID_ \
      -std=c++11
 
-
 LOCAL_LDFLAGS += -Wl,--export-dynamic
 
 ## Includes
@@ -49,7 +45,4 @@ LOCAL_MODULE := libloc_pla
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PRELINK_MODULE := false
-
 include $(BUILD_SHARED_LIBRARY)
-endif # not BUILD_TINY_ANDROID
