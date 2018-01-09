a315b9cd534d9e3ddb1d3b4cc1dadb743990739a
Mikhail Naganov <mnaganov@google.com>
2018-01-08 17:05:28 -0800

    msm8998-common: Uprev soundtrigger to 2.1
    Bug: 68823037
    Change-Id: If634345594ea6d574f49d58e29b48165c3b9458b
    Test: "ok google" and music iq from locked screen

--- msm8998.mk
+++ msm8998.mk
@@ -98,7 +98,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.soundtrigger@2.1-impl
 
 PRODUCT_PACKAGES += \
     tinymix
