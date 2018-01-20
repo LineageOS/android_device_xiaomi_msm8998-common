30ddd5559115b90a36f015f7999f532bd792a73b
LuK1337 <priv.luk@gmail.com>
2018-01-20 16:35:15 +0100

    msm8998-common: Move readmac to /vendor
    Change-Id: Ifea0f1c649717a5b1ea50449e6447e40cf00791a

--- readmac/Android.mk
+++ readmac/Android.mk
@@ -4,6 +4,7 @@ ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE           := readmac
 LOCAL_MODULE_TAGS      := optional
+LOCAL_VENDOR_MODULE    := true
 LOCAL_SRC_FILES        := xiaomi_readmac.c
 LOCAL_CFLAGS           += -Wall -Werror
 LOCAL_SHARED_LIBRARIES := liblog
