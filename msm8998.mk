8c26ed83c2352ec5dca57f0063a6e13b80e408ff
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    msm8998-common: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- msm8998.mk
+++ msm8998.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
