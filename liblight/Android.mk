0f7bb8aa5666b9d3331e7265e21549e1c3d23bd1
Michael Bestas <mkbestas@lineageos.org>
2018-02-21 00:26:38 +0200

    msm8998-common: Make copyright header style consistent
    Change-Id: I26b75a0b7226897ee8b9bbed341588d591055fda

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 LOCAL_PATH:= $(call my-dir)
 
