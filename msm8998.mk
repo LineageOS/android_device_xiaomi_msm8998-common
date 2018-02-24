c92d0505f4148079f4315afe271e7f0dc166978f
Demon Singur <demonsingur@gmail.com>
2018-02-24 10:04:25 +0000

    msm8998-common: import media seccomp policy from MIUI 8.2.1
    Change-Id: I2e82d02417e033e06f78a51186bd47740fbe510f

--- msm8998.mk
+++ msm8998.mk
@@ -346,6 +346,11 @@ PRODUCT_PACKAGES += \
     libxml2 \
     libprotobuf-cpp-full
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
+    $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.msm8998
