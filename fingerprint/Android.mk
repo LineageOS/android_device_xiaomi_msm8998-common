bdd00c4e3a908d8940c8b7ffacf2a78eb1be2d12
Demon Singur <demonsingur@gmail.com>
2018-03-04 17:25:12 +0000

    msm8998-common: turn fingerprint wrapper intro a hidl service
    Change-Id: Ib72892417d401947aeb3935ef0f018c3b5daf94b

--- fingerprint/Android.mk
+++ fingerprint/Android.mk
@@ -1,33 +1,21 @@
-#
-# Copyright (C) 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
+LOCAL_MODULE := android.hardware.biometrics.fingerprint@2.1-service.msm8998
+LOCAL_INIT_RC := android.hardware.biometrics.fingerprint@2.1-service.msm8998.rc
+LOCAL_PROPRIETARY_MODULE := true
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SRC_FILES := \
-    FingerprintWrapper.cpp
+    BiometricsFingerprint.cpp \
+    service.cpp \
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog
+    libcutils \
+    liblog \
+    libhidlbase \
+    libhidltransport \
+    libhardware \
+    libutils \
+    android.hardware.biometrics.fingerprint@2.1 \
 
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := fingerprint.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_MULTILIB := 64
-
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_EXECUTABLE)
