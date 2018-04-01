91f40355d7437bd5ca487ecca76af83ee41c7517
Michael Bestas <mkbestas@lineageos.org>
2018-04-01 23:35:44 +0300

    msm8998-common: Update to oreo NXP NFC
    Change-Id: I866c2cce2296544dd6515554a7bee19bc10fd527

--- msm8998.mk
+++ msm8998.mk
@@ -283,8 +283,12 @@ PRODUCT_PACKAGES += \
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
+    com.gsma.services.nfc \
     com.nxp.nfc.nq \
+    com.nxp.nfc.nq.xml \
+    libnqnfc-nci \
     nfc_nci.nqx.default \
+    libp61-jcop-kit \
     NQNfcNci \
     nqnfcee_access.xml \
     nqnfcse_access.xml \
@@ -292,7 +296,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service
+    vendor.nxp.hardware.nfc@1.0-impl \
+    vendor.nxp.hardware.nfc@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf
