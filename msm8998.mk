d65633d61ce60a73deadc288629f403cba04ceed
Demon Singur <demonsingur@gmail.com>
2018-04-11 14:22:55 +0000

    msm8998-common: remove AOSP bluetooth impl
    We use the QTI impl which properly loads the bluetooth
    firmware, remove the AOSP one.
    
    Keep using AOSP service because QTI service depends on ANT+
    which doesn't work.
    
    Change-Id: Ie7a31c60c89890567a02bd92adb84eb03eacaf56

--- msm8998.mk
+++ msm8998.mk
@@ -140,7 +140,6 @@ PRODUCT_PACKAGES += \
     libbt-vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service
 
 # Connectivity Engine support (CNE)
