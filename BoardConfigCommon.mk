c47748eb585f5250a607397dcb0333e96abfcf48
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-16 22:34:25 -0700

    msm8998-common: enable cpuidle during boot
    This is only used to keep cpuidle disabled during boot, which is
    unnecessary and doesn't really improve boot times (but does incur a high
    power cost).
    
    Change-Id: Id5878edd58a59de36da9bf9381262cabd3f3cb4b
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,7 +50,6 @@ BOARD_KERNEL_PAGESIZE := 4096
 
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
-BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 
