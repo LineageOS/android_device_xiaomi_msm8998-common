92c2470f1c240f7c2befbec0d937394443cf008a
Demon Singur <demonsingur@gmail.com>
2018-06-21 16:50:54 +0000

    msm8998-common: move device specific props to vendor
    Change-Id: I6cd9774441fd6b78bdc98150e48c20c68f887e0e

---
+++ vendor_prop.mk
@@ -0,0 +1,193 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+	aaudio.hw_burst_min_usec=2000 \
+	aaudio.mmap_exclusive_policy=2 \
+	aaudio.mmap_policy=2 \
+	af.fast_track_multiplier=2 \
+	audio.adm.buffering.ms=6 \
+	audio.deep_buffer.media=true \
+	audio.offload.min.duration.secs=15 \
+	audio.offload.video=true \
+	persist.audio.dirac.speaker=true \
+	persist.audio.speaker.dualmode=true \
+	persist.vendor.audio.fluence.speaker=true \
+	persist.vendor.audio.fluence.voicecall=true \
+	persist.vendor.audio.fluence.voicerec=false \
+	persist.vendor.audio.ras.enabled=false \
+	persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+	ro.config.vc_call_vol_steps=11 \
+	ro.vendor.audio.sdk.fluencetype=fluence \
+	ro.vendor.audio.sdk.ssr=false \
+	vendor.audio.dolby.ds2.enabled=false \
+	vendor.audio.dolby.ds2.hardbypass=false \
+	vendor.audio.flac.sw.decoder.24bit=true \
+	vendor.audio.hw.aac.encoder=false \
+	vendor.audio.noisy.broadcast.delay=600 \
+	vendor.audio.offload.buffer.size.kb=32 \
+	vendor.audio.offload.gapless.enabled=true \
+	vendor.audio.offload.multiaac.enable=true \
+	vendor.audio.offload.multiple.enabled=true \
+	vendor.audio.offload.passthrough=false \
+	vendor.audio.offload.pstimeout.secs=3 \
+	vendor.audio.offload.track.enable=false \
+	vendor.audio.parser.ip.buffer.size=0 \
+	vendor.audio.safx.pbe.enabled=true \
+	vendor.audio.tunnel.encode=false \
+	vendor.audio.use.sw.alac.decoder=true \
+	vendor.audio.use.sw.ape.decoder=true \
+	vendor.audio_hal.period_size=192
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+	qcom.bluetooth.soc=cherokee \
+	ro.bluetooth.hfp.ver=1.6
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.camera.HAL3.enabled=1 \
+	persist.camera.set.afd=4 \
+	persist.camera.xm.green.b=0.96 \
+	persist.camera.xm.green.r=0.97 \
+	persist.dualcam.lpm.enable=1 \
+	vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.miui.cit,org.lineageos.snap \
+	vidc.enc.dcvs.extra-buff-count=2
+
+# CNE
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.cne.feature=1
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+	debug.gralloc.enable_fb_ubwc=1 \
+	debug.sf.enable_hwc_vds=1 \
+	debug.sf.hw=1 \
+	debug.sf.latch_unsignaled=1 \
+	ro.hwui.drop_shadow_cache_size=7 \
+	ro.hwui.gradient_cache_size=1 \
+	ro.hwui.layer_cache_size=64 \
+	ro.hwui.path_cache_size=39 \
+	ro.hwui.r_buffer_cache_size=12 \
+	ro.hwui.text_large_cache_height=2048 \
+	ro.hwui.text_large_cache_width=3072 \
+	ro.hwui.text_small_cache_height=2048 \
+	ro.hwui.text_small_cache_width=2048 \
+	ro.hwui.texture_cache_flushrate=0.4 \
+	ro.hwui.texture_cache_size=96 \
+	ro.opengles.version=196610 \
+	ro.sf.lcd_density=480 \
+	vendor.display.enable_default_color_mode=1
+
+# DPM
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.vendor.dpm.feature=1 \
+	persist.vendor.dpm.nsrm.bkg.evt=3955
+
+# FRP
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.frp.pst=/dev/block/platform/soc/1da4000.ufshc/by-name/frp
+
+# IMS
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.dbg.volte_avail_ovr=1 \
+	persist.dbg.vt_avail_ovr=1 \
+	persist.vendor.qti.telephony.vt_cam_interface=1
+
+# Memory
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.heapgrowthlimit=256m \
+	dalvik.vm.heapmaxfree=8m \
+	dalvik.vm.heapminfree=4m \
+	dalvik.vm.heapsize=512m \
+	dalvik.vm.heapstartsize=16m \
+	dalvik.vm.heaptargetutilization=0.75
+
+# Network manager
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.data.netmgrd.qos.enable=true \
+	persist.data.wda.enable=true \
+	persist.data.df.agg.dl_pkt=10 \
+	persist.data.df.agg.dl_size=4096 \
+	persist.data.df.dev_name=rmnet_usb0 \
+	persist.data.df.dl_mode=5 \
+	persist.data.df.iwlan_mux=9 \
+	persist.data.df.mux_count=8 \
+	persist.data.df.ul_mode=5
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vendor.extension_library=libqti-perfd-client.so \
+	ro.vendor.qti.core_ctl_max_cpu=4 \
+	ro.vendor.qti.core_ctl_min_cpu=0 \
+	ro.vendor.qti.sys.fw.bg_apps_limit=60 \
+	sdm.debug.disable_skip_validate=1 \
+	sdm.perf_hint_window=50
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.data.mode=concurrent \
+	persist.radio.apm_sim_not_pwdn=1 \
+	persist.radio.dynamic_sar=false \
+	persist.radio.multisim.config=dsds \
+	persist.rmnet.data.enable=true \
+	persist.vendor.radio.custom_ecc=1 \
+	persist.vendor.radio.force_on_dc=true \
+	persist.vendor.radio.rat_on=combine \
+	persist.vendor.radio.redir_party_num=1 \
+	persist.vendor.radio.report_codec=1 \
+	persist.vendor.radio.sib16_support=1 \
+	rild.libpath=/vendor/lib64/libril-wrapper.so \
+	ro.telephony.default_cdma_sub=0 \
+	ro.telephony.default_network=22,22 \
+	ro.use_data_netmgrd=true \
+	telephony.lteOnCdmaDevice=1 \
+	vendor.voice.path.for.pcm.voip=true
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.sys.sensors.cfos=false \
+	persist.sys.sensors.pug=false \
+	persist.sys.sensors.spg=false \
+	ro.qti.sensors.taptap_gesture=true \
+	ro.vendor.sdk.sensors.gestures=false \
+	ro.vendor.sensors.cmc=false \
+	ro.vendor.sensors.dev_ori=false \
+	ro.vendor.sensors.dpc=true \
+	ro.vendor.sensors.facing=false \
+	ro.vendor.sensors.iod=false \
+	ro.vendor.sensors.mot_detect=true \
+	ro.vendor.sensors.multishake=true \
+	ro.vendor.sensors.pmd=false \
+	ro.vendor.sensors.sta_detect=true
+
+# Time services
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.delta_time.enable=true
+
+# Vendor security patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.vendor.build.security_patch=2018-03-01
+
+# WFD
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.debug.wfd.enable=1 \
+	persist.sys.wfd.virtual=0
+
+# Wlan
+PRODUCT_PROPERTY_OVERRIDES += \
+	wifi.interface=wlan0
