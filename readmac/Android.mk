660229415184789210ca225d02e8a0ef11e1d161
Demon Singur <demonsingur@gmail.com>
2018-06-23 11:06:54 +0000

    msm8998-common: readmac: fix build with VNDK=current
    Change-Id: I145b26d5842429e56fff2286257dfccc4cea411b

--- readmac/Android.mk
+++ readmac/Android.mk
@@ -1,6 +1,5 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
-ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
 LOCAL_MODULE           := readmac
 LOCAL_MODULE_TAGS      := optional
@@ -8,5 +7,5 @@ LOCAL_VENDOR_MODULE    := true
 LOCAL_SRC_FILES        := xiaomi_readmac.c
 LOCAL_CFLAGS           += -Wall -Werror
 LOCAL_SHARED_LIBRARIES := liblog libcutils
+LOCAL_HEADER_LIBRARIES := libcutils_headers
 include $(BUILD_EXECUTABLE)
-endif
