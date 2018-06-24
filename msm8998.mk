20eacf607f0f645862c35fc1a66004943c82c783
Demon Singur <demonsingur@gmail.com>
2018-06-24 13:28:07 +0000

    msm8998-common: remove duplicate NFC comment
    Change-Id: Ieaab1a5da99af0e19f8a1bd488d99076d6dc3de2

--- msm8998.mk
+++ msm8998.mk
@@ -281,7 +281,6 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.0-impl \
     android.hardware.nfc@1.0-service
 
-# NFC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-brcm.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-brcm.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
