0f1b5163c18b64ef6c044bb91be64fd43d8a1535
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    msm8998-common: Do not explicitly include camera.device implementations
    camera.device@1.0-impl and camera.device@3.2-impl are dependencies
    of camera provider therefore there's no need to add them to PRODUCT_PACKAGES.
    
    Change-Id: I3ae211965a31b5f85af3dd0fa1c7811b802586c3

--- msm8998.mk
+++ msm8998.mk
@@ -133,9 +133,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Bluetooth
 PRODUCT_PACKAGES += \
