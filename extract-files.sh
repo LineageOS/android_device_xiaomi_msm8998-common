09b50a60856ba3ce43622997f41abab3b6eebeea
Michael Bestas <mkbestas@lineageos.org>
2018-07-05 19:27:45 +0300

    msm8998-common: sed xml files during extract instead of pinning
    Change-Id: Ic54747c759f965afccab37c48336c3d27cb186e9

--- extract-files.sh
+++ extract-files.sh
@@ -92,4 +92,16 @@ skia_to_misk "$CAMERA_MSM8998"
 #
 sed -i "s|system/etc/dualcamera.png|vendor/etc/dualcamera.png|g" "$MI_CAMERA_HAL"
 
+#
+# Correct VZW IMS library location
+#
+QTI_VZW_IMS_INTERNAL="$COMMON_BLOB_ROOT"/vendor/etc/permissions/qti-vzw-ims-internal.xml
+sed -i "s|/system/vendor/framework/qti-vzw-ims-internal.jar|/vendor/framework/qti-vzw-ims-internal.jar|g" "$QTI_VZW_IMS_INTERNAL"
+
+#
+# Correct android.hidl.manager@1.0-java jar name
+#
+QTI_LIBPERMISSIONS="$COMMON_BLOB_ROOT"/vendor/etc/permissions/qti_libpermissions.xml
+sed -i "s|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g" "$QTI_LIBPERMISSIONS"
+
 "$MY_DIR"/setup-makefiles.sh
