b755e6c54b7cccc24f91f8e276dddc4205756d4d
Bruno Martins <bgcngm@gmail.com>
2018-07-07 22:40:59 +0100

    msm8998-common: Remove implicitly built QTI camera shared libraries
     * These shared libs are built when TARGET_USES_QTI_CAMERA_DEVICE
       flag is enabled.
    
    Change-Id: Ifb39b7c2c8de2cbb1f59c6dc492a3763d32f1b7a

--- msm8998.mk
+++ msm8998.mk
@@ -131,9 +131,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    android.hardware.camera.provider@2.4-service
 
 # Bluetooth
 PRODUCT_PACKAGES += \
