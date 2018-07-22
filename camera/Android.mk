40457579e56f0074a4ab1568e063177618d2645c
Demon Singur <demonsingur@gmail.com>
2018-07-22 20:10:40 +0000

    msm8998-common: add static copy of libminikin in libcamshim
    This fixes symbols mismatch when using O-built vendor on P GSI.
    
    libminikin is not vendor-available so we need to hack our way
    around the build system.
    
    Change-Id: Ieb682d28c015fbdf863146e335f55ea317b961b1

--- camera/Android.mk
+++ camera/Android.mk
@@ -3,7 +3,28 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libcamshim
 LOCAL_MULTILIB := 32
-LOCAL_VENDOR_MODULE := true
 LOCAL_SRC_FILES := camera.c
 LOCAL_MODULE_TAGS := optional
+LOCAL_WHOLE_STATIC_LIBRARIES := \
+    libminikin
+LOCAL_SHARED_LIBRARIES := \
+    libharfbuzz_ng \
+    libft2 \
+    libz \
+    libutils \
+    liblog \
+    libicuuc
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcamshim.vendor
+LOCAL_REQUIRED_MODULES := libcamshim
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_PREBUILT_MODULE_FILE := $(2ND_TARGET_OUT_INTERMEDIATE_LIBRARIES)/libcamshim.so
+LOCAL_STRIP_MODULE := false
+LOCAL_MULTILIB := 32
+LOCAL_MODULE_TAGS := optional
+LOCAL_INSTALLED_MODULE_STEM := libcamshim.so
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_VENDOR_MODULE := true
+include $(BUILD_PREBUILT)
