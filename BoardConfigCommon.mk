a45d383015b9941f763fac79dec1a75dd256512d
Demon Singur <demonsingur@gmail.com>
2018-08-02 13:44:19 +0000

    msm8998-common: switch to QCOM HIDL Blueooth stack
    Change-Id: I264a39fc889b02632ac070cf24fde665e19adeec

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -55,7 +55,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=2048
 TARGET_BOARD_PLATFORM := msm8998
 
 # ANT+
-BOARD_ANT_WIRELESS_DEVICE := "qualcomm-uart"
+BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
 # Assert
 TARGET_BOARD_INFO_FILE := $(PLATFORM_PATH)/board-info.txt
