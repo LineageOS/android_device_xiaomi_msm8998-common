2d451234804a6d22217b2279bd0f13f2d6603c82
Demon Singur <demonsingur@gmail.com>
2018-08-08 19:57:04 +0000

    msm8998-common: recovery: include moved headers
    Change-Id: Ia0e49562dac01bf3ecd674270f2011546cfd3498

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := bootable/recovery
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_xiaomi
 LOCAL_MODULE_TAGS := eng
+LOCAL_STATIC_LIBRARIES := libedify libotautil
 include $(BUILD_STATIC_LIBRARY)
