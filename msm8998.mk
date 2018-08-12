21afd62ef2e140304f38f54e0927b8ddff1440bd
LuK1337 <priv.luk@gmail.com>
2018-08-12 11:49:14 +0200

    DNM: msm8998-common: Disable telephony-ext for now
    Change-Id: Ia0ae303a786640f2535cae0ab2dfcd041569bca4

--- msm8998.mk
+++ msm8998.mk
@@ -337,13 +337,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
-# Telephony
-PRODUCT_PACKAGES += \
-    telephony-ext
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
