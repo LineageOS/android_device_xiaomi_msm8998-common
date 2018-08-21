9950aa6c32b795863435b12f908c5513a5fc1c37
Demon Singur <demonsingur@gmail.com>
2018-08-21 08:35:06 +0000

    msm8998-common: fork android.hardware.camera.common@1.0-helper implementation
    Change-Id: Idd86413ea330898db27cad86fad78fcd5ae7f2e6

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,5 +1,33 @@
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAN_VARS)
+LOCAL_MODULE := android.hardware.camera.common@1.0-helper
+LOCAL_MULTILIB := 32
+LOCAL_C_INCLUDES := \
+    system/media/private/camera/include \
+    $(LOCAL_PATH)/include
+LOCAL_SRC_FILES := \
+    CameraModule.cpp \
+    CameraMetadata.cpp \
+    CameraParameters.cpp \
+    VendorTagDescriptor.cpp \
+    HandleImporter.cpp
+LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libbase \
+    libcutils \
+    liblog \
+    libhidlbase \
+    libhidltransport \
+    libhwbinder \
+    libutils \
+    libhardware \
+    libcamera_metadata \
+    android.hardware.graphics.mapper@2.0
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_SHARED_LIBRARY)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libcamshim
 LOCAL_MULTILIB := 32
