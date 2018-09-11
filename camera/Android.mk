2c7475d504ddab90c283c7be7acfa5a5edf3f076
Demon Singur <demonsingur@gmail.com>
2018-09-11 06:24:40 +0000

    Revert "msm8998-common: shim missing skia symbols"
    Get rid of the camera shim.
    
    This reverts commit c08c53b82b146419dc9f7fb4e39ed7a00dde095c.
    
    Change-Id: I6b6ba80f7c1d58e60e5ed4f105512343eccae8cb

--- camera/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libcamshim
-LOCAL_MULTILIB := 32
-LOCAL_VENDOR_MODULE := true
-LOCAL_SRC_FILES := camera.c
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
--- msm8998.mk
+++ msm8998.mk
@@ -129,9 +129,6 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    libcamshim
-
-PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
