29d733e7addf457d825a4cb95c279dad5e6dc68b
Demon Singur <demonsingur@gmail.com>
2018-09-11 06:24:40 +0000

    Revert "msm8998-common: shim missing skia symbols"
    Get rid of the camera shim.
    
    This reverts commit c08c53b82b146419dc9f7fb4e39ed7a00dde095c.
    
    Change-Id: I6b6ba80f7c1d58e60e5ed4f105512343eccae8cb

--- extract-files.sh
+++ extract-files.sh
@@ -73,12 +73,6 @@ CAMERA2_SENSOR_MODULES="$COMMON_BLOB_ROOT"/vendor/lib/libmmcamera2_sensor_module
 sed -i "s|/system/etc/camera/|/vendor/etc/camera/|g" "$CAMERA2_SENSOR_MODULES"
 
 #
-# Replace libminikin.so with libcamshim.so to allow shimming
-#
-CAMERA_MSM8998="$COMMON_BLOB_ROOT"/vendor/lib/hw/camera.msm8998.so
-sed -i "s|libminikin.so|libcamshim.so|g" "$CAMERA_MSM8998"
-
-#
 # Load camera watermark from vendor
 #
 MI_CAMERA_HAL="$COMMON_BLOB_ROOT"/vendor/lib/libMiCameraHal.so
