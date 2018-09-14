22af580c8e10593c12a47d3bf6e0b41dca013f36
Demon Singur <demonsingur@gmail.com>
2018-09-14 19:47:39 +0000

    DNM: msm8998-common: disable camera packages
    Change-Id: Ia2d1e25469f17edeb4ef6cfc19216c89c2ac2eb6

--- msm8998.mk
+++ msm8998.mk
@@ -124,18 +124,6 @@ PRODUCT_COPY_FILES += \
 #PRODUCT_COPY_FILES += \
 #    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    Snap
-
-PRODUCT_PACKAGES += \
-    libcamshim.vendor \
-    libicuuc.vendor
-
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
