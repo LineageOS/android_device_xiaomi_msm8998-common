9d84acece52eddb1154baf7ac61d4097aca9a4c4
Anton Hansson <hansson@google.com>
2018-12-12 16:15:21 +0000

    msm8998-common: Remove PRODUCT_AAPT config
    This config strips unused drawable resources from the device
    to save space, but it is not particularly effective in practice
    (roughly ~30 MB across all partitions).
    
    Remove it to make the system image more generic.
    
    Bug: 118676832
    Test: make
    Change-Id: I3ae120773e793af6e7b5759d383b7fa9c65aeb48

--- msm8998.mk
+++ msm8998.mk
@@ -71,10 +71,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
-
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
