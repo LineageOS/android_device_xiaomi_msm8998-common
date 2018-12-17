12f5abefb7baae740c18b84bfe39d1d49aa2269a
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    msm8998-common: Adding Audio HAL V5: Direction API
    Bug: 119137468
    Test: With testbed app
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa

--- msm8998.mk
+++ msm8998.mk
@@ -106,8 +106,8 @@ PRODUCT_PACKAGES += \
     libvolumelistener \
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.soundtrigger@2.1-impl
 
