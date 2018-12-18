fbdb2ad6d22a7827eca3ecba1bc7c99e9d78c3a9
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    msm8998-common: Enabling sound trigger V2_2 for msm8998 targets
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c

--- msm8998.mk
+++ msm8998.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
     tinymix
