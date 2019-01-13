0709cc81713adf3fe4a92ccc6804ca0180291158
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    msm8998-common: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>

--- msm8998.mk
+++ msm8998.mk
@@ -352,6 +352,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # VNDK
 PRODUCT_PACKAGES += \
     vndk_package
