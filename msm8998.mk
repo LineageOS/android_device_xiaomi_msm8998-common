c90d2a39f65f7424a113511f809c7fd14b22d770
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    msm8998-common: Migrate to livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1

--- msm8998.mk
+++ msm8998.mk
@@ -231,9 +231,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_msm8998
 
-# LiveDisplay native
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
