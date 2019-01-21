d32a9cc9d1e04dcaf3404a0661b75691a06f1aa6
Vol Zhdanov <wight554@gmail.com>
2019-01-21 20:16:51 +0000

    msm8998-common: decommonize density and color mode props
    * chiron's default color profile is 0
    * xiaomi overrides chiron density by miui specific prop
    
    Change-Id: If8f5cb4d75714c9d5c677e33a288dd8fbb440ca1

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -79,10 +79,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     debug.sf.latch_unsignaled=1 \
     ro.opengles.version=196610 \
-    ro.sf.lcd_density=480 \
     vendor.display.disable_rotator_downscale=1 \
     vendor.display.disable_skip_validate=1 \
-    vendor.display.enable_default_color_mode=1 \
     vendor.display.perf_hint_window=50 \
     vendor.gralloc.enable_fb_ubwc=1
 
