708b59a0e35f380a718e2d6ad8f05265587c80fa
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    msm8998-common: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- msm8998.mk
+++ msm8998.mk
@@ -16,6 +16,9 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
