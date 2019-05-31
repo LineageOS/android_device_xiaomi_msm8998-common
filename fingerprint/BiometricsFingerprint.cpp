4a1c27d2b4776bbc460e9dd8ec5b063bcf7c6e26
dianlujitao <dianlujitao@lineageos.org>
2019-05-31 16:06:44 +0800

    msm8998-common: Update IFAA for pie blobs
    Change-Id: I2801cc3633416a1552c07230b68cf1d83b484edc
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- fingerprint/BiometricsFingerprint.cpp
+++ fingerprint/BiometricsFingerprint.cpp
@@ -257,7 +257,7 @@ fingerprint_device_t* getDeviceForVendor(const char *class_name)
 }
 
 void setFpVendorProp(const char *fp_vendor) {
-    property_set("persist.sys.fp.vendor", fp_vendor);
+    property_set("persist.vendor.sys.fp.vendor", fp_vendor);
     property_set("ro.boot.fpsensor", fp_vendor);
 }
 
