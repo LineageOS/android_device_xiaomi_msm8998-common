4cd72310a1c9738b8880017309d7627ff541f40d
Ramjee Singh <ramjee@codeaurora.org>
2019-07-03 15:03:56 +0530

    msm8998-common: Enable frame ctl for aac codec format
    Change-Id: Id43ab138deb892e564ed710e4dd5c9b644852f44

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -30,6 +30,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.voicerec=false \
     persist.vendor.audio.ras.enabled=false \
     persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    persist.vendor.bt.aac_frm_ctl.enabled=true \
     ro.config.media_vol_steps=25 \
     ro.config.vc_call_vol_steps=11 \
     ro.vendor.audio.sdk.fluencetype=fluence \
