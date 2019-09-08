7e4a3d9092c2a78f1985428d163dda4f9d27f7f1
Erfan Abdi <erfangplus@gmail.com>
2019-09-08 15:04:55 +0430

    msm8998-common: ims: Make shim for libui
    P: GraphicBuffer::lock(uint32_t inUsage, void** vaddr);
    Q: GraphicBuffer::lock(uint32_t inUsage, void** vaddr, int32_t* outBytesPerPixel = nullptr, int32_t* outBytesPerStride = nullptr);
    Error: cannot locate symbol "_ZN7android13GraphicBuffer4lockEjPPv"
    
    Change-Id: I1c1fe66f3f73149ced601eae87b970ce392d50ee

--- extract-files.sh
+++ extract-files.sh
@@ -65,6 +65,9 @@ function blob_fixup() {
     product/etc/permissions/qcrilhook.xml)
         sed -i 's|/system/framework/qcrilhook.jar|/system/product/framework/qcrilhook.jar|g' "${2}"
         ;;
+    product/lib64/lib-imsvideocodec.so)
+        patchelf --add-needed "libui_shim.so" "${2}"
+        ;;
     vendor/etc/permissions/qti_libpermissions.xml)
         sed -i 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
         ;;
