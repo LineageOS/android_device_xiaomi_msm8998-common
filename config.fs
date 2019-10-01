00b79347d4e4a8da25ddd858a4c0efeb256d5721
Michael Bestas <mkbestas@lineageos.org>
2019-10-01 21:26:08 +0300

    msm8998-common: Sync config.fs with LA.UM.7.4.r1-05000-8x98.0
    * Add a few missing services that we previously removed
    
    Change-Id: I22fd4f062194df4925cb5c6499c4c2f2eed63ce4

--- config.fs
+++ config.fs
@@ -1,6 +1,27 @@
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
+[AID_VENDOR_QDSS]
+value:2902
+
+[AID_VENDOR_RFS]
+value:2903
+
+[AID_VENDOR_RFS_SHARED]
+value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
+
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
 [vendor/bin/pm-service]
 mode: 0755
 user: AID_SYSTEM
@@ -42,6 +63,12 @@ mode: 0755
 user:  AID_GPS
 group: AID_GPS
 caps: SETUID SETGID
+
+[vendor/bin/sensors.qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
 
 [firmware/]
 mode: 0771
