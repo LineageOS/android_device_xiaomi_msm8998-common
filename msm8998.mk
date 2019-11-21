b1dbb54c06894ca9cbf9c5815f02fcbc8d6c8c5b
Arian <arian.kulmer@web.de>
2019-11-21 18:04:50 +0100

    Revert "msm8998-common: Create system copy of ims-ext-common and add to classpath"
    This reverts commit ac7ed0006bcd8b4fa65d97b0deb3df65c5b29de3.
    
    Change-Id: I7f2ca904d9a180b24d2994828a79218a9b4f4175

--- msm8998.mk
+++ msm8998.mk
@@ -225,10 +225,7 @@ PRODUCT_PACKAGES += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common_system
-
-PRODUCT_BOOT_JARS += \
-    ims-ext-common_system
+    ims-ext-common
 
 # IRQ
 PRODUCT_COPY_FILES += \
