01ff592381a5369e8a0a953e6a1fd5b78fd7b668
Arian <arian.kulmer@web.de>
2019-12-01 14:32:53 +0100

    msm8998-common: vndk: Enable runtime isolation
    Change-Id: I027556b1a760242da957b9c3a9c270bd9f20def7
    Co-authored-by: Angga Satya Putra <anggasp@hotmail.com>

--- msm8998.mk
+++ msm8998.mk
@@ -365,6 +365,12 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libdng_sdk.vendor_32 \
+    libstdc++.vendor_32 \
+    vndk-ext
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
