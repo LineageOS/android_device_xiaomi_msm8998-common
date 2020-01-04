bf1ef5d31b591e926ac782044ebd08473fb4e16b
Michael Bestas <mkbestas@lineageos.org>
2020-01-04 20:56:13 +0200

    msm8998-common: Reenable HFP
    * Looks like this lib was opensource so we can just compile it
    
    Change-Id: I61d2815346f6177204967a6ec2c05123ce5c1bc6

--- msm8998.mk
+++ msm8998.mk
@@ -104,10 +104,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libaudio-resampler \
+    libhfp \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
-    libvolumelistener \
+    libvolumelistener
 
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
