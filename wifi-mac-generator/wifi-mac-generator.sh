4ee1a945593ff5d8de7f689b75d9b373f0841177
Arian <arian.kulmer@web.de>
2020-01-05 22:59:58 +0100

    msm8998-common: wifi-mac-generator: Adapt for our needs
    Change-Id: I09eb6b627d89b3da7245a12c53c42d983d275578

--- wifi-mac-generator/wifi-mac-generator.sh
+++ wifi-mac-generator/wifi-mac-generator.sh
@@ -1,42 +1,19 @@
 #!/vendor/bin/sh
 
-# data format:
-# wlan0=a89cedb9c066
-WLAN_MAC_DATA_PATH="/data/vendor/mac_addr/wlan_mac.bin"
+WLAN_MAC="/mnt/vendor/persist/wlan_bt/wlan.mac"
 
 # data format:
 # Intf0MacAddress=00AA00BB00CC
 # Intf1MacAddress=00AA00BB00CD
 # END
-WLAN_MAC_PERSIST_PATH="/mnt/vendor/persist/wlan_mac.bin"
+WLAN_MAC_BIN="/mnt/vendor/persist/wlan_mac.bin"
 
-function wait_for_file() {
-    file="${1}"
-    max_retries=10
-    retries=0
-
-    while [ ! -s "${file}" ]; do
-        retries=$((retries + 1))
-
-        if [ "${retries}" -eq "${max_retries}" ]; then
-            return 1
-        fi
-
-        sleep 1
-    done
-
-    return 0
-}
-
-if ! wait_for_file "${WLAN_MAC_DATA_PATH}"; then
+if [ ! -s "${WLAN_MAC}" ]; then
     exit
 fi
 
-# Read file contents
-raw_mac_data=$(cat "${WLAN_MAC_DATA_PATH}")
-
-# Strip wlan0= from the string
-raw_mac="${raw_mac_data#*=}"
+# Read the mac from persist
+raw_mac=$(od -An -t x1 $WLAN_MAC | tr -d '\n ')
 
 # Convert to decimal
 dec_mac=$(printf "%d" "0x$raw_mac")
@@ -53,6 +30,6 @@ dec_mac=$(echo "$dec_mac + 1" | bc)
 second_mac=$(printf "%08X" "$dec_mac")
 
 # Write the MACs
-echo "Intf0MacAddress=${first_mac}" > "${WLAN_MAC_PERSIST_PATH}"
-echo "Intf1MacAddress=${second_mac}" >> "${WLAN_MAC_PERSIST_PATH}"
-echo "END" >> "${WLAN_MAC_PERSIST_PATH}"
+echo "Intf0MacAddress=${first_mac}" > "${WLAN_MAC_BIN}"
+echo "Intf1MacAddress=${second_mac}" >> "${WLAN_MAC_BIN}"
+echo "END" >> "${WLAN_MAC_BIN}"
