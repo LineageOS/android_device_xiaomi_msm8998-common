44e51efd9c231e256722cbd3e74d096f4394ea46
Arian <arian.kulmer@web.de>
2020-01-05 22:59:58 +0100

    msm8998-common: wifi-mac-generator: Adapt to read the mac from persist
    * Xiaomi stores the wlan mac at /mnt/vendor/persist/wlan_bt/wlan.mac in hex,
      Instead of reading it from NV we can use that.
    * Therefore add back the symlink from /mnt/vendor/persist/wlan_mac.bin to
      /vendor/firmware/wlan/qca_cld/wlan_mac.bin so that qcacld can read the mac.
    * Change the user/group to system, according to teh file permissions of
      /mnt/vendor/persist/wlan_bt/wlan.mac
    
    Change-Id: I09eb6b627d89b3da7245a12c53c42d983d275578

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/vendor/bin/sh
 
 # data format:
-# hex binary file containing the mac
-WLAN_MAC_HEX_PATH="/mnt/vendor/persist/wlan_bt/wlan.mac"
+# Intf0MacAddress=00AA00BB00CC
+WLAN_MAC_HEX_PATH="wlan.mac"
 
 # data format:
 # Intf0MacAddress=00AA00BB00CC
 # Intf1MacAddress=00AA00BB00CD
 # END
-WLAN_MAC_PERSIST_PATH="/mnt/vendor/persist/wlan_mac.bin"
+#WLAN_MAC_PERSIST_PATH="wlan_mac.bin"
 
 if [ ! -s "${WLAN_MAC_HEX_PATH}" ]; then
     exit
@@ -32,7 +32,7 @@ dec_mac=$(echo "$dec_mac + 1" | bc)
 second_mac=$(printf "%012X" "$dec_mac")
 
 # Write the MACs
-echo "Intf0MacAddress=${first_mac}" > "${WLAN_MAC_PERSIST_PATH}"
-echo "Intf1MacAddress=${second_mac}" >> "${WLAN_MAC_PERSIST_PATH}"
-echo "END" >> "${WLAN_MAC_PERSIST_PATH}"
+echo "Intf0MacAddress=${first_mac}"
+echo "Intf1MacAddress=${second_mac}"
+echo "END"
 
