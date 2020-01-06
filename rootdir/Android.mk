59053364974612e4fc94d01fc5ec165da6bd0c99
Arian <arian.kulmer@web.de>
2020-01-06 21:22:23 +0100

    msm8998-common: Stop caring about fingerprint data migration
    * Everyone should have migrated now.
    
    This reverts commit 90b5b6942700740a89d0f97569f7f904af05783f.
    
    Change-Id: I1d546227f2e3f9e4de285e5ab6ab533aaf1b13d0

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -90,11 +90,3 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := bin/move_widevine_data.sh
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := move_fingerprint_data.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := bin/move_fingerprint_data.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
-include $(BUILD_PREBUILT)
