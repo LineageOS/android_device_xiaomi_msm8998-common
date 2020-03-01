da0c208d51816118b1a91c00db25b85239d03007
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:34:32 -0800

    msm8998-common: Remove unnecessary netutils packages
    * These are already packaged by AOSP.
    
    Change-Id: I77f91fbbb33a6d891d14fe852b094ab914ca3111

--- msm8998.mk
+++ msm8998.mk
@@ -288,9 +288,7 @@ PRODUCT_PACKAGES += \
 
 # Net
 PRODUCT_PACKAGES += \
-    android.system.net.netd@1.0 \
-    android.system.net.netd@1.1.vendor \
-    libandroid_net
+    android.system.net.netd@1.1.vendor
 
 # NFC
 PRODUCT_PACKAGES += \
