d5c65c502b602fd27e77a58e4bb9dff150b8528c
Arian <arian.kulmer@web.de>
2020-09-12 12:25:17 +0200

    msm8998-common: SELinux permissive
    Change-Id: I9fd2f437f6a3a6e1e977f69cce58d9b30ef5e318

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -53,6 +53,7 @@ BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Platform
 TARGET_BOARD_PLATFORM := msm8998
