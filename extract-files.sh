330f2b4558b1a7be2644adea55d79c21c499cda4
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 01:17:48 +0200

    msm8998-common: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- extract-files.sh
+++ extract-files.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -22,9 +12,9 @@ set -e
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${LINEAGE_ROOT}/vendor/lineage/build/tools/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -34,8 +24,19 @@ source "${HELPER}"
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
+ONLY_COMMON=
+ONLY_TARGET=
+KANG=
+SECTION=
+
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
+        --only-common )
+                ONLY_COMMON=true
+                ;;
+        --only-target )
+                ONLY_TARGET=true
+                ;;
         -n | --no-cleanup )
                 CLEAN_VENDOR=false
                 ;;
@@ -59,73 +60,71 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-    lib64/libwfdnative.so)
-        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
-        ;;
-    system_ext/etc/init/dpmd.rc)
-        sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
-        ;;
-    system_ext/etc/permissions/com.qti.dpmframework.xml)
-        ;&
-    system_ext/etc/permissions/dpmapi.xml)
-        sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
-        ;;
-    system_ext/etc/permissions/qcrilhook.xml)
-        sed -i 's|/product/framework/qcrilhook.jar|/system/system_ext/framework/qcrilhook.jar|g' "${2}"
-        ;;
-    system_ext/lib64/lib-imsvideocodec.so)
-        "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
-        ;;
-    system_ext/lib64/libdpmframework.so)
-        "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
-        ;;
-    vendor/etc/permissions/qti_libpermissions.xml)
-        sed -i 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
-        ;;
-    vendor/lib/hw/camera.msm8998.so)
-        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
-        ;;
-    vendor/lib/libFaceGrade.so)
-        "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib/libMiCameraHal.so)
-        "${PATCHELF}" --replace-needed "libicuuc.so" "libicuuc-v28.so" "${2}"
-        "${PATCHELF}" --replace-needed "libminikin.so" "libminikin-v28.so" "${2}"
-        ;;
-    vendor/lib/libarcsoft_beauty_shot.so)
-        "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib/libicuuc-v28.so)
-        "${PATCHELF}" --set-soname "libicuuc-v28.so" "${2}"
-        ;;
-    vendor/lib/libminikin-v28.so)
-        "${PATCHELF}" --set-soname "libminikin-v28.so" "${2}"
-        ;;
-    vendor/lib/libmmcamera2_sensor_modules.so)
-        sed -i 's|/data/misc/camera/camera_lsc_caldata.txt|/data/vendor/camera/camera_lsc_calib.txt|g' "${2}"
-        ;;
-    vendor/lib/libmmcamera2_stats_modules.so)
-        "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib/libmpbase.so)
-        "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
-        ;;
+        lib64/libwfdnative.so)
+            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+            ;;
+        system_ext/etc/init/dpmd.rc)
+            sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
+            ;;
+        system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml)
+            sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
+            ;;
+        system_ext/etc/permissions/qcrilhook.xml)
+            sed -i 's|/product/framework/qcrilhook.jar|/system/system_ext/framework/qcrilhook.jar|g' "${2}"
+            ;;
+        system_ext/lib64/lib-imsvideocodec.so)
+            "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            ;;
+        system_ext/lib64/libdpmframework.so)
+            "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
+            ;;
+        vendor/etc/permissions/qti_libpermissions.xml)
+            sed -i 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
+            ;;
+        vendor/lib/hw/camera.msm8998.so)
+            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+            ;;
+        vendor/lib/libFaceGrade.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        vendor/lib/libMiCameraHal.so)
+            "${PATCHELF}" --replace-needed "libicuuc.so" "libicuuc-v28.so" "${2}"
+            "${PATCHELF}" --replace-needed "libminikin.so" "libminikin-v28.so" "${2}"
+            ;;
+        vendor/lib/libarcsoft_beauty_shot.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        vendor/lib/libicuuc-v28.so)
+            "${PATCHELF}" --set-soname "libicuuc-v28.so" "${2}"
+            ;;
+        vendor/lib/libminikin-v28.so)
+            "${PATCHELF}" --set-soname "libminikin-v28.so" "${2}"
+            ;;
+        vendor/lib/libmmcamera2_sensor_modules.so)
+            sed -i 's|/data/misc/camera/camera_lsc_caldata.txt|/data/vendor/camera/camera_lsc_calib.txt|g' "${2}"
+            ;;
+        vendor/lib/libmmcamera2_stats_modules.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        vendor/lib/libmpbase.so)
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
     esac
 }
 
-# Initialize the helper for common device
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true "${CLEAN_VENDOR}"
+if [ -z "${ONLY_TARGET}" ]; then
+    # Initialize the helper for common device
+    setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-        "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+fi
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
+if [ -z "${ONLY_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${LINEAGE_ROOT}" false "${CLEAN_VENDOR}"
+    setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" \
-            "${KANG}" --section "${SECTION}"
+    extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
 
 "${MY_DIR}/setup-makefiles.sh"
