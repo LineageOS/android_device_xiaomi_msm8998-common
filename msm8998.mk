51badf52a472e7babbb64be5ba052d2273b69bb0
dianlujitao <dianlujitao@lineageos.org>
2021-02-09 18:14:04 +0800

    msm8998-common: Switch to source built vendor.qti.hardware.perf@1.0
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    Change-Id: I1dc96e4a51c4c81dd847c59a58ec670d89054f64

--- msm8998.mk
+++ msm8998.mk
@@ -313,7 +313,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@1.0.vendor
 
 # Protobuf
 PRODUCT_PACKAGES += \
