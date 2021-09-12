5908d891aed47aec1c6ab1c55ba67b38c83c88f8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 19:28:33 +0200

    msm8998-common: Remove explicit camera HIDL interfaces from targets list
    * They're built automatically since newer revision needs them
    
    Change-Id: I695bb8d7c4120add9c8583658de5ae73f508a25c

--- msm8998.mk
+++ msm8998.mk
@@ -140,9 +140,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
     android.frameworks.displayservice@1.0.vendor \
-    android.hardware.camera.device@3.4:64 \
     android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4:64 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.6:64 \
