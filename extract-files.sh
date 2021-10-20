24c4058b484a88dd4ba063c3ffa4b602627e84b4
Erfan Abdi <erfangplus@gmail.com>
2021-10-20 17:20:19 +0300

    msm8998-common: libshims: Add shim for camera blobs
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I5e622e68422f5c4da13c0752a7eb3f4b20ddae49

--- extract-files.sh
+++ extract-files.sh
@@ -72,7 +72,15 @@ function blob_fixup() {
         system_ext/etc/permissions/qti_libpermissions.xml)
             sed -i 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
             ;;
+        system_ext/lib64/lib-imscamera.so)
+            for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
+            done
+            ;;
         system_ext/lib64/lib-imsvideocodec.so)
+            for LIBGUI_SHIM in $(grep -L "libgui_shim.so" "${2}"); do
+                "${PATCHELF}" --add-needed "libgui_shim.so" "${LIBGUI_SHIM}"
+            done
             for LIBUI_SHIM in $(grep -L "libui_shim.so" "${2}"); do
                 "${PATCHELF}" --add-needed "libui_shim.so" "$LIBUI_SHIM"
             done
