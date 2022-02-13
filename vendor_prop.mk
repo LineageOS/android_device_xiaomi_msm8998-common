03646f100d17ddc83b94913649f338a2f6e87dc0
clarencelol <clarencekuiek@icloud.com>
2022-02-13 20:06:23 +0800

    msm8998-common: properties: Disable SDM Scalar
    * Else, ScalarConfig: :GetPipeScaleSettingsInfo: lib_scale_get_pipe_settings failed: status = 2
    
    Change-Id: I7ee49c90f7ad948641d81b3c261351a6b3728fed

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -126,6 +126,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     vendor.display.disable_partial_split=1 \
     vendor.display.disable_rotator_downscale=1 \
+    vendor.display.disable_scaler=1 \
     vendor.display.disable_skip_validate=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.display.perf_hint_window=50 \
