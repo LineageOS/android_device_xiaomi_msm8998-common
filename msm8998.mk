c6368dda859636d868f7b121dae2fab85a507770
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:29:35 +0200

    msm8998-common: Stop explicitely building netutils-wrapper-1.0
    * Built by default
    
    Change-Id: Id6666535bb8d0b84d2d0a8d19411fafa49df3d68

--- msm8998.mk
+++ msm8998.mk
@@ -283,8 +283,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     android.system.net.netd@1.1.vendor \
-    libandroid_net \
-    netutils-wrapper-1.0
+    libandroid_net
 
 # NFC
 PRODUCT_PACKAGES += \
