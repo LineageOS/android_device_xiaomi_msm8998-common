1e0caab04cb4df3f900567cf7d7f3cbcb5b22d28
Bruno Martins <bgcngm@gmail.com>
2022-09-22 00:40:01 +0100

    msm8998-common: Drop component overrides
    This is no longer needed as of Android 13.
    
    Change-Id: I38435c47c8781678f82014decbbae40dcab0a07b

--- msm8998.mk
+++ msm8998.mk
@@ -157,10 +157,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5:64 \
     vendor.qti.hardware.camera.device@1.0:64
 
-# Component overrides
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
-
 # Control groups and task profiles
 PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_28.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
