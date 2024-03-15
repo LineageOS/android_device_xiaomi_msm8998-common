c5f49255df3f861933dffe03fa8448e18c29ff12
Yumi Yukimura <me.cafebabe@gmail.com>
2024-03-15 15:17:25 +0800

    Revert "msm8998-common: Switch to legacy WiFi HIDL HAL"
    * WiFi AIDL HAL works in U QPR2
    
    This reverts commit 935ca09e75f916ba79b20ef263683a2276008d31.
    
    Change-Id: I4d38fcc7e5999f934344bc364afb4f4d8a049711

--- msm8998.mk
+++ msm8998.mk
@@ -421,7 +421,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service.legacy
+    android.hardware.wifi-service
 
 PRODUCT_PACKAGES += \
     wifi-mac-generator
