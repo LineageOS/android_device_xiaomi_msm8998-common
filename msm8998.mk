5529dd495f6d1606012731bcf85ae98f5efd6198
Georg Veichtlbauer <georg@vware.at>
2024-04-03 23:24:33 +0200

    msm8998-common: Switch blob source to FP3
    Change-Id: Ic23eb57d1f0b6d89c36ac3f4a4ea8d6bfcf7dd56

--- msm8998.mk
+++ msm8998.mk
@@ -122,7 +122,8 @@ PRODUCT_COPY_FILES += \
 
 # ANT+
 PRODUCT_PACKAGES += \
-    AntHalService
+    AntHalService-Soong \
+    com.dsi.ant@1.0.vendor
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -130,7 +131,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default \
-    vendor.qti.hardware.btconfigstore@1.0.vendor
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -141,7 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4:64 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5:64 \
+    android.hardware.camera.provider@2.6:64 \
     libcamera_shim \
     libpiex_shim \
     vendor.qti.hardware.camera.device@1.0:64
