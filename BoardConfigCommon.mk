dd1d3f53e09d4f8b1786d0b1a2f75cb69277b111
Michael Bestas <mikeioannina@gmail.com>
2024-06-15 00:46:19 +0300

    msm8998-common: Include lineage FCM
    Change-Id: Ib4408e941d920a3a38132ba1c8360994f5534c0b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,7 +79,8 @@ BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(PLATFORM_PATH)/framework_manifest.xml
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
