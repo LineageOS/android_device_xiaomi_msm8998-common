9283d8285867735cee357871dc3ad31bc86cb27b
LuK1337 <priv.luk@gmail.com>
2024-06-20 17:19:19 -0400

    msm8998-common: light: Redo alpha sanity check
    Change-Id: I08f335eba806fa10792fe5a9c348cc59327b3b4d

--- light/Lights.cpp
+++ light/Lights.cpp
@@ -73,9 +73,9 @@ static uint32_t getBrightness(const HwLightState& state) {
     blue = state.color & 0xFF;
 
     /*
-     * Scale RGB brightness if Alpha brightness is not 0xFF.
+     * Scale RGB brightness if Alpha brightness is within the proper range (0-255).
      */
-    if (alpha != 0xFF) {
+    if (alpha > 0 && alpha < 255)
         red = red * alpha / 0xFF;
         green = green * alpha / 0xFF;
         blue = blue * alpha / 0xFF;
