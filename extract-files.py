a8c2fbc923be1fdd694ef2296e648cbb3aca791b
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 08:30:58 +0300

    msm8998-common: fix vendor.qti.hardware.fm@1.0 ELF checks
    Change-Id: I43f0824f88c24dd9d039f9075090c17f4266ab2d

--- extract-files.py
+++ extract-files.py
@@ -37,6 +37,7 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
+        'vendor.qti.hardware.fm@1.0.so',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     (
