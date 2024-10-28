fa9933f89cb6d705183437b93087d8756ff60d17
Nanhumly <admin@warbler.cc>
2024-10-28 10:33:08 +0000

    msm8998-common: fix dirac_resource ELF checks
    Change-Id: I525eeba17cd4f613c185c5d330bfc08e3fd4022a

--- extract-files.py
+++ extract-files.py
@@ -37,6 +37,7 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
+        'dirac_resource.so',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
     (
