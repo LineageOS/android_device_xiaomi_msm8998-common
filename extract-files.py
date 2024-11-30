95c01182dbf6748fd5adbbe1849e0f856e776cf7
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-29 23:46:02 -0500

    msm8998-common: Patch lib-imscamera with libgui_shim as well
    * Was fine, as the lib that loads this is patched, but lets
      appease elfchecks.
    
    Change-Id: Ic97316811e3364c36b744bde21237137c2b7e6d6

--- extract-files.py
+++ extract-files.py
@@ -18,6 +18,8 @@ from extract_utils.main import (
 )
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/lib-imscamera.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
     'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
         .add_needed('libgui_shim.so')
         .replace_needed('libqdMetaData.so', 'libqdMetaData.system.so'),
