7c0140f987af674f689a440a96bb2e3f74374b06
Nanhumly <admin@warbler.cc>
2025-01-05 07:54:19 +0000

    msm8998-common: Move non-A/B target flag to BoardConfigCommon.mk
    Change-Id: Ie1780f07aff2bd573f1d0470886075b4e8f6a28f

--- msm8998.mk
+++ msm8998.mk
@@ -399,9 +399,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qmi_fw.conf:$(TARGET_COPY_OUT_VENDOR)/etc/qmi_fw.conf
 
-# Updater
-AB_OTA_UPDATER := false
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
