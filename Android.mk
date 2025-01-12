f14d8d1bf056c75701142cb1555c0daa10ce95e3
Bruno Martins <bgcngm@gmail.com>
2025-01-12 09:05:07 +0000

    msm8998-common: Drop all Android makefiles inclusion within the tree
    Change-Id: I33a163d7243fe1203538d6e37a907acd3976f1d4

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017-2024 The LineageOS Project
+# Copyright (C) 2017-2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,10 +14,7 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := $(call my-dir)
-
 ifneq ($(filter sagit chiron,$(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
